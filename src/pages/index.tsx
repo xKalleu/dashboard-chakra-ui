c976526e9adf31d23652159a1a52b447d020ee66
Kalleu Oliveira <xkalleu@gmail.com>
2021-04-12 22:27:39 -0300

    add: header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-import Head from 'next/head';
+import { Flex, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
 
-export default function Home() {
-	return <h1>Opa</h1>;
+export default function SignIn() {
+	return (
+		<Flex w="100vw" h="100vh" align="center" justify="center">
+			<Flex
+				as="form"
+				w="100%"
+				maxWidth={360}
+				bg="gray.800"
+				p="8"
+				borderRadius="8"
+				flexDir="column">
+				<Stack spacing="4">
+					<Input type="email" label="E-mail" name="email" />
+					<Input type="password" label="Senha" name="password" />
+				</Stack>
+				<Button type="submit" mt="6" colorScheme="pink" size="lg">
+					Entrar
+				</Button>
+			</Flex>
+		</Flex>
+	);
 }
