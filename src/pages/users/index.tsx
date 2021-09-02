d78b1ba538c3930ca4a7c62ae4a804d6ee331621
Kalleu Oliveira <xkalleu@gmail.com>
2021-09-01 22:17:14 -0300

    add: create hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,33 +16,16 @@ import {
 	Spinner
 } from '@chakra-ui/react';
 import Link from 'next/link';
-import React, { useEffect } from 'react';
-import { useQuery } from 'react-query';
+import React from 'react';
 
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-	const { data, isLoading, error } = useQuery('users', async () => {
-		const response = await fetch('http://localhost:3000/api/users');
-		const data = await response.json();
-
-		const users = data.users.map((user) => {
-			return {
-				id: user.id,
-				name: user.name,
-				email: user.email,
-				createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-					day: '2-digit',
-					month: 'long',
-					year: 'numeric'
-				})
-			};
-		});
-		return users;
-	});
+	const { data, isLoading, isFetching, error } = useUsers();
 
 	const isWideVersion = useBreakpointValue({
 		base: false,
@@ -60,6 +43,9 @@ export default function UserList() {
 					<Flex mb="8" justify="space-between" align="center">
 						<Heading size="lg" fontWeight="normal">
 							Usuários
+							{!isLoading && isFetching && (
+								<Spinner size="sm" color="gray.500" ml="4" />
+							)}
 						</Heading>
 						<Link href="/users/create" passHref>
 							<Button
